ffd05ebd2a83eac9c0ecfc76758b3f1546f9e638
SamreenAsif <l201292@lhr.nu.edu.pk>
2023-04-02 09:17:41 +0500

    final-ver

--- github-search/src/App.js
+++ github-search/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { SearchUser } from './pages/SearchUser/SearchUser';
 
 function App() {
   return (
     <div className="App">
-      
+      <SearchUser/>
     </div>
   );
 }
---
+++ github-search/src/pages/SearchUser/SearchUser.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import "./SearchUser.css";
+
+export const SearchUser = () => {
+  const [username, setUsername] = useState("");
+  const [result, setResults] = useState([]); // state for storing data received from api
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("search-btn").click();
+    }
+  };
+
+  const handleSearchInputChange = (event) => {
+   
+    setUsername(event.target.value);
+  };
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        `https://api.github.com/search/users?q=${username}`,
+        {
+          headers: {
+            Accept: "application/vnd.github.v3+json",
+          },
+        }
+      );
+      console.log("Response status:", response.status);
+      console.log("Response headers:", response.headers);
+      const data = await response.json();
+      console.log("Response body:", data);
+      setResults(data.items);
+    } catch (error) {
+      console.error("Error occurred while fetching data from API:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="header">
+        <h1>GitHub Search</h1>
+      </div>
+      <form className="search-bar">
+        <input
+          type="text"
+          name="Search"
+          placeholder="Search..."
+          id="search-text"
+          value={username}
+          onKeyDown={handleKeyDown}
+          onChange={handleSearchInputChange}
+        />
+        <button id="search-btn" onClick={handleSearch}>
+          SEARCH
+        </button>
+      </form>
+
+      {/* <div className="container">
+        {result.map((user) => {
+          <div className="profile" key={user.id}>
+            <img src="{user.avatar_url}" id="profile-img" alt="{user.login}" />
+          </div>;
+        })}
+      
+      <div className="user-info">
+      {result.map((user) => {
+        <p>
+          <label htmlFor="name">Name : </label> {user.login}
+        </p>
+          
+        })}
+        </div>
+      </div> */}
+      {result.length > 0 && (
+        <div>
+          {result.map((user) => (
+            <div className="container" key={user.id}>
+              <div className="profile">
+                <img src={user.avatar_url} id="profile-img" alt={user.login} />
+              </div>
+              <div className="user-info">
+                <p>
+                  <label htmlFor="name" id="user-name">
+                    Name :{" "}
+                  </label>{" "}
+                  {user.login}
+                </p>
+                <p>
+                  <label htmlFor="link" id="user-account-link">
+                    Link :{" "}
+                  </label>
+                  <a href={user.html_url}>{user.html_url}</a>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
